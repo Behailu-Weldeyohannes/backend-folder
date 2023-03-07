18fbe03cf415f461e888296c65e79a1939b32d3c
Behailu Weldeyohannes <bweldeyohannes@gmail.com>
2023-03-07 17:52:41 -0500

    intro

---
+++ 01-intro/dtypes.py
@@ -0,0 +1,17 @@
+# data is collected in the prefered format
+# numeric (integer, float, complex number)
+# sequence (string, list, tuples)
+# dictionary
+# boolean
+# set
+
+# a = 10
+
+# print(type(a))
+
+# b = 2.3
+
+# print(type(b))
+
+d = [1,2,3,4]
+print(type(d))
---
+++ 01-intro/hello_world.py
@@ -0,0 +1,13 @@
+print("Hello World")
+
+x = 1 + 2 \
++3
+print(x)  
+
+name = "John"
+if name == "John":
+    print(name)
+
+    # Variables
+
+
