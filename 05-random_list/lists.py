31190a32dabfdb9de493daddbe13b6976a71ae0a
Behailu Weldeyohannes <bweldeyohannes@gmail.com>
2023-04-27 00:06:03 -0400

    list and random

---
+++ 05-random_list/lists.py
@@ -0,0 +1,11 @@
+# states=  ["Delaware", "Texas", "Virginia", "Maryland", "N Carolina"]
+
+# print(states[2])
+# print(states[-2])
+
+# states[0] = "S Carolina"
+
+# states.append("Washington DC")
+# states.extend(["Behailu", "Kenya"])
+
+# print(states)
---
+++ 05-random_list/randomList.py
@@ -0,0 +1,18 @@
+# import my_module
+
+# print(my_module.pi)
+# import random
+# random_integer = random.randint(1, 10)
+# random_float = random.random()
+# print(random_float)
+
+# print(random_integer)
+
+# heads or tails
+
+# random_side = random.randint(0, 1)
+
+# if random_side ==1:
+#     print("Heads")
+# else:
+#     print("Tails")
