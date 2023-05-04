cfee515e9ca6e85aaeed785177e5bf07c5d6e0fa
Behailu Weldeyohannes <bweldeyohannes@gmail.com>
2023-05-04 17:27:58 -0400

    comprhensions

--- 10-zipUnzip/zip.py
+++ 10-zipUnzip/zip.py
@@ -7,21 +7,21 @@
 # num,let,nam =zip(*combo)
 # print(num,let,nam)
 
-keys = 'this parrot is deceased'
-values = 'denna papegojan är avliden'
+# keys = 'this parrot is deceased'
+# values = 'denna papegojan är avliden'
 
-keys =  keys.split()
-values = values.split()
+# keys =  keys.split()
+# values = values.split()
 
-print(keys, values)
+# print(keys, values)
 
-en_sv_dict = dict(zip(keys, values))
-print(en_sv_dict)
+# en_sv_dict = dict(zip(keys, values))
+# print(en_sv_dict)
 
-new_dict = {key:value for key, value in zip(keys, values)}
+# new_dict = {key:value for key, value in zip(keys, values)}
 
-print(new_dict)
-en,sv = list(en_sv_dict.keys()),list(en_sv_dict.values())
-print(en,sv)
-en1,sv1 = zip(*en_sv_dict.items())
-print(en1,sv1)
+# print(new_dict)
+# en,sv = list(en_sv_dict.keys()),list(en_sv_dict.values())
+# print(en,sv)
+# en1,sv1 = zip(*en_sv_dict.items())
+# print(en1,sv1)
