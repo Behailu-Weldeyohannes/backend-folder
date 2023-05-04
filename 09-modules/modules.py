e8591955f5684803ae6b050d97fba8f1254ec71a
Behailu Weldeyohannes <bweldeyohannes@gmail.com>
2023-05-03 22:28:54 -0400

    modules

---
+++ 09-modules/modules.py
@@ -0,0 +1,12 @@
+
+# datetime random string os math browser
+
+# import platform as pl
+# from platform import python_version, system
+# # print(pl.python_version())
+# print(python_version())
+# print(system())
+
+# from platform import python_version as pv
+
+# print(pv())
