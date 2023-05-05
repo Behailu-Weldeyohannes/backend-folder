f130f7e7acce3ad422cf43ac2c895f544c4e5a33
Behailu Weldeyohannes <bweldeyohannes@gmail.com>
2023-05-04 21:03:07 -0400

    random..

---
+++ 13-randomness/random.py
@@ -0,0 +1,10 @@
+# import random
+# for i in range(5):
+#     print(random.randrange(2,100,2))
+
+# friends_list = ['John', 'Eric', 'Michael', 'Terry', 'Graham']
+# print(random.choice(friends_list))
+# print(random.sample(friends_list, 3))
+
+# random.shuffle(friends_list)
+# print(friends_list)
